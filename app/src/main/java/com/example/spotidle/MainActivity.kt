23ccda9ee9e66116ca231b275256c7564dab642e
PottierLoic <loic.pottier@live.fr>
2024-09-28 18:20:21 +0200

    basic app

--- app/src/main/java/com/example/spotidle/MainActivity.kt
+++ app/src/main/java/com/example/spotidle/MainActivity.kt
@@ -13,13 +13,35 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.spotidle.ui.theme.SpotidleTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.painterResource
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Phone // used for music guess
 import androidx.compose.material.icons.filled.Star // used for lyrics guess
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.AddCircle // used for album guess
 import androidx.compose.material.icons.filled.Person // used for artist guess
-import androidx.compose.ui.graphics.Color
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Close // used for paause button
+import android.media.MediaPlayer
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.platform.LocalContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -93,28 +115,243 @@ fun BottomNavigationBar(
 }
 
 @Composable
+fun SpotifightTitle(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Spotifight",
+            color = Color(0xFF1ED760),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+        Image(
+            painter = painterResource(id = R.drawable.spotify_logo),
+            contentDescription = "Spotify Logo",
+            modifier = Modifier
+                .size(32.dp)
+                .padding(end = 8.dp),
+            contentScale = ContentScale.Fit
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GuessInputField(
+    label: String,
+    inputText: String,
+    onInputChange: (String) -> Unit,
+    onGuessSubmit: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = inputText,
+            onValueChange = onInputChange,
+            label = { Text(label) },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true,
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.White
+            )
+        )
+        Button(
+            onClick = onGuessSubmit,
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text(text = "Submit Guess")
+        }
+    }
+}
+@Composable
+fun GuessSection(
+    modifier: Modifier = Modifier,
+    correctGuessName: String,
+    onGuessSubmit: (String) -> Unit = {},
+) {
+    var inputText by remember { mutableStateOf("") }
+    val guesses = remember { mutableStateListOf<String>() }
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        GuessInputField(
+            label = "Enter the album name",
+            inputText = inputText,
+            onInputChange = { inputText = it },
+            onGuessSubmit = {
+                if (inputText.isNotBlank()) {
+                    guesses.add(inputText)
+                    onGuessSubmit(inputText)
+                    inputText = ""
+                }
+            }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(text = "Previous Guesses:", color = Color.White, fontWeight = FontWeight.Bold)
+
+        guesses.reversed().forEach { guess ->
+            val isCorrect = guess.equals(correctGuessName, ignoreCase = true)
+            val backgroundColor = if (isCorrect) Color(0xFF00C853) else Color(0xFFD50000)
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 4.dp)
+                    .background(backgroundColor)
+                    .padding(8.dp)
+            ) {
+                Text(text = guess, color = Color.White)
+            }
+        }
+    }
+}
+
+@Composable
 fun MusicGuessScreen(modifier: Modifier = Modifier) {
-    Text(text = "", modifier = modifier.fillMaxSize())
+    val correctSongName = "Ratio" // TODO REMOVE
+    val context = LocalContext.current
+    val mediaPlayer = remember { MediaPlayer.create(context, R.raw.testsong) }
+    var isPlaying by remember { mutableStateOf(false) }
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        SpotifightTitle()
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                if (isPlaying) {
+                    mediaPlayer.pause()
+                } else {
+                    mediaPlayer.start()
+                }
+                isPlaying = !isPlaying
+            },
+            modifier = Modifier
+                .size(64.dp)
+        ) {
+            Icon(
+                imageVector = if (isPlaying) Icons.Default.Close else Icons.Default.PlayArrow, // TODO: change icon to pause icon
+                contentDescription = if (isPlaying) "Pause" else "Play"
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        GuessSection(correctGuessName=correctSongName)
+    }
 }
 
 @Composable
 fun LyricsGuessScreen(modifier: Modifier = Modifier) {
-    Text(text = "", modifier = modifier.fillMaxSize())
+    val correctSongName = "TO REPLACE" // TODO REMOVE
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        SpotifightTitle()
+        Spacer(modifier = Modifier.height(16.dp))
+        // TODO: lyrics here
+        Spacer(modifier = Modifier.height(16.dp))
+        GuessSection(correctGuessName=correctSongName)
+    }
 }
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    Text(text = "", modifier = modifier.fillMaxSize())
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        SpotifightTitle()
+        // TODO: home screen content here
+    }
 }
 
 @Composable
 fun AlbumGuessScreen(modifier: Modifier = Modifier) {
-    Text(text = "", modifier = modifier.fillMaxSize())
+    val correctAlbumName = "Quand la musique est bonne" // TODO REMOVE
+    var blurAmount by remember { mutableFloatStateOf(25f) }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        SpotifightTitle()
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.jjgalbumcover),
+            contentDescription = "Album Cover",
+            modifier = Modifier
+                .size(240.dp)
+                .blur(radiusX = blurAmount.dp, radiusY = blurAmount.dp)
+                .padding(end = 8.dp),
+            contentScale = ContentScale.Fit
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        GuessSection(
+            correctGuessName = correctAlbumName,
+            onGuessSubmit = { guess ->
+                if (guess.equals(correctAlbumName, ignoreCase = true)) {
+                    blurAmount = 0f
+                } else {
+                    blurAmount = (blurAmount - 5f).coerceAtLeast(0f)
+                }
+            }
+        )
+    }
 }
 
 @Composable
 fun ArtistGuessScreen(modifier: Modifier = Modifier) {
-    Text(text = "", modifier = modifier.fillMaxSize())
+    val correctArtistName = "TO REPLACE" // TODO REMOVE
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        SpotifightTitle()
+        Spacer(modifier = Modifier.height(16.dp))
+        // TODO artist content here
+        Spacer(modifier = Modifier.height(16.dp))
+        GuessSection(correctGuessName=correctArtistName)
+    }
 }
 
 @Preview(showBackground = true)
