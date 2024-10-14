6bf743614de9859a34649aaad7f00369cbb0286c
Lucccyo <cha.git@mailo.fr>
2024-10-14 14:11:31 +0200

    feat: fetch suggestions

--- app/src/main/java/com/example/spotidle/spotifyApiManager/UserManager.kt
+++ app/src/main/java/com/example/spotidle/spotifyApiManager/UserManager.kt
@@ -17,7 +17,10 @@ class UserManager() {
     suspend fun getLikedTracksIds(): List<String> {
         var offset = 0
         val limit = 50
-        val likedTracks = mutableListOf<String>()
+        val ids = mutableListOf<String>()
+        val names = mutableListOf<String>()
+        val artists = mutableListOf<String>()
+        val albums = mutableListOf<String>()
         return try {
             while(true) {
                 val response = RetrofitInstance.api.getUserLikedTracks(
@@ -25,11 +28,14 @@ class UserManager() {
                     limit,
                     offset
                 )
-                likedTracks.addAll(response.items.map { it.track.id })
+                ids.addAll(response.items.map { it.track.id })
+                names.addAll(response.items.map { it.track.name })
+                artists.addAll(response.items.map { it.track.album.name })
+                albums.addAll(response.items.map { it.track.artists[0].name })
                 if (response.next == null) { break }
                 offset += limit
             }
-            return likedTracks.toList()
+            return listOf(ids.toList(), names.toList(), artists.toList(), albums.toList())
         } catch (e: Exception) {
             Log.e("Spotify", "Failed to fetch user liked track: ${e.message}")
             emptyList<String>()
